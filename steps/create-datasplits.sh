567bbca891312e4a51cb05f18f5a8f12ef98b0f7
preethi <preethi>
2015-08-30 23:50:51 -0500

    create-datasplits.sh

--- steps/create-datasplits.sh
+++ steps/create-datasplits.sh
@@ -35,12 +35,12 @@ eval)
 esac
 
 mkdir -p "$(dirname "$ids_file")"
-mkdir -p "$(dirname "$split_ids_file")"
+mkdir -p "$(dirname "$splitids_file")"
 for L in ${LANG[@]}; do
 	full_lang_name=`awk '/'${L}'/ {print $2}' $langmap`;
 	sed -e 's:.wav::' $LISTDIR/$full_lang_name/${dtype}
 done > $ids_file
-split -n r/$nparallel $ids_file  $tmpdir/split-$dtype.
+split -n r/$nparallel $ids_file  $tmpdir/split-${dtype}.
 for i in `seq 1 $nparallel`; do
-	mv `ls $tmpdir/split-$dtype.* | head -1` ${split_ids_file}.$i
+	mv `ls $tmpdir/split-${dtype}.* | head -1` ${splitids_file}.$i
 done
