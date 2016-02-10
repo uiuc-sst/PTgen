319863505c42fc26693c8d2b92c7c63aeae20de0
camilleg <cog@illinois.edu>
2016-02-10 14:32:01 -0600

    c++ idioms

--- src/extra-utils.h
+++ src/extra-utils.h
@@ -1,72 +1,68 @@
+// Utility functions.
+
 #ifndef FST_EXTRA_UTILS_H__
 #define FST_EXTRA_UTILS_H__
 
+// Although this file doesn't need OpenFST itself, its callers typically do.
 #include <fst/compat.h>
 #include <fst/types.h>
+
 #include <algorithm>
-#include <iostream>
+#include <cmath>
 #include <fstream>
+#include <iostream>
 #include <string>
-#include <math.h>
 
-	//************************
-	//USEFUL UTILITY FUNCTIONS
-	//************************
-	
-	// trim from start
-	 static inline std::string &ltrim(std::string &s) {
-	   s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-	   return s;
-	 }
+// (In .h files, "using std::string" etc is unsafe.)
 
-	 // trim from end
-	 static inline std::string &rtrim(std::string &s) {
-	   s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-	   return s;
-	 }
+// Trim leading whitespace.
+static inline std::string &ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+  return s;
+}
 
-	 // trim from both ends
-	 static inline std::string &trim(std::string &s) {
-	   s.erase(std::remove(s.begin(), s.end(), '\n'), s.end());
-	   return ltrim(rtrim(s));
-	 }
+// Trim trailing whitespace.
+static inline std::string &rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+  return s;
+}
 
-	 static inline std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
-	   std::stringstream ss(s);
-	   std::string item;
-	   if(s.empty())
-	     return elems;
-	   while(std::getline(ss, item, delim)) {
-	     elems.push_back(item);
-	   }
-	   return elems;
-	 }
+// Trim whitespace from both ends.
+static inline std::string& trim(std::string &s) {
+  s.erase(std::remove(s.begin(), s.end(), '\n'), s.end());
+  return ltrim(rtrim(s));
+}
 
-	 static inline std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems, int num_of_splits) {
-	   std::stringstream ss(s);
-	   std::string item;
-	   if(s.empty())
-	     return elems;
-	   int num_of_elems;
-	   while(std::getline(ss, item, delim)) {
-             num_of_elems++;
-	     elems.push_back(item);
-             if(num_of_elems == num_of_splits)
-	       break;
-	   }
-	   return elems;
-	 }
+static inline std::vector<std::string> &split(const std::string &s, const char delim, std::vector<std::string> &elems) {
+  if (s.empty())
+    return elems;
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim))
+    elems.push_back(item);
+  return elems;
+}
 
-	 // split using a delimiter
-	 static inline std::vector<std::string> split(const std::string &s, char delim) {
-	   std::vector<std::string> elems;
-	   return split(s, delim, elems);
-	 }
+static inline std::vector<std::string> &split(const std::string &s, const char delim, std::vector<std::string> &elems, const int num_splits) {
+  if (s.empty())
+    return elems;
+  std::stringstream ss(s);
+  std::string item;
+  for (int num_elems = 0; num_elems < num_splits && std::getline(ss, item, delim); ++num_elems)
+    elems.push_back(item);
+  return elems;
+}
 
-	 //split into specified number of splits using a delimiter
-	 static inline std::vector<std::string> split(const std::string &s, char delim, int num_of_splits) {
-	   std::vector<std::string> elems;
-	   return split(s, delim, elems, num_of_splits);
-	 }
+// Split using a delimiter.
+static inline std::vector<std::string> split(const std::string &s, const char delim) {
+  std::vector<std::string> elems;
+  return split(s, delim, elems);
+}
+
+// Split using a delimiter, into at most n parts.
+static inline std::vector<std::string> split(const std::string &s, const char delim, const int num_splits) {
+  std::vector<std::string> elems;
+  return split(s, delim, elems, num_splits);
+}
 
 #endif // FST_EXTRA_UTILS_H__
