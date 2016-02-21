33814b16180af10a71dd5555f5dd4fa017ba8e7d
camilleg <cog@illinois.edu>
2016-02-21 10:11:54 -0600

    c++isms

--- src/compute_turker_similarity.cc
+++ src/compute_turker_similarity.cc
@@ -7,7 +7,7 @@
 #include <tr1/unordered_map>
 #include "extra-utils.h"
 
-#define MAXTURKERS 15
+const int MAXTURKERS = 15;
 
 using namespace std;
 using namespace std::tr1;
@@ -24,7 +24,7 @@ float editdistance(const char* a, const char* b) {
 	stringstream ss1(str1);
 	string item1,item2,result;
 	// Substitution and Insertion/Deletion costs.
-	float scost = 1, idcost = 1;
+	float scost = 1.0, idcost = 1.0;
 	while (getline(ss1, item1, ' ')) {
 		std::remove_copy_if(item1.begin(), item1.end(),            
 			std::back_inserter(result), //Store output without punctuations
@@ -41,32 +41,32 @@ float editdistance(const char* a, const char* b) {
 		result = "";
 	}
 
-	int len1 = wrds1.size();
-	int len2= wrds2.size();
+	const int len1 = wrds1.size();
+	const int len2 = wrds2.size();
 	if(len1 == 0)
-		return(idcost*len2);
+		return idcost*len2;
 	if(len2 == 0)
-		return(idcost*len1);
+		return idcost*len1;
 	float** dist_matrix = new float* [len1+1];
 	for(int i = 0; i <= len1; i++)
 		dist_matrix[i] = new float[len2+1];
 	for(int i = 0; i <= len1; i++) {
 		for(int j = 0; j <= len2; j++) {
-			dist_matrix[i][j] = 0;
-			dist_matrix[0][j] = (j*idcost);
+			dist_matrix[i][j] = 0.0;
+			dist_matrix[0][j] = j*idcost;
 		}
-		dist_matrix[i][0] = (i*idcost);
+		dist_matrix[i][0] = i*idcost;
 	}
 
 	//Dynamic programming algorithm to compute edit distance between two word sequences
 	for(int i = 1; i <= len1; i++) {
 		for(int j = 1; j <= len2; j++) {
-			float cost = (wrds1[i-1] == wrds2[j-1]) ? 0 : scost;
-			float mn = std::min(dist_matrix[i-1][j]+idcost,dist_matrix[i][j-1]+idcost); 
+			const float cost = (wrds1[i-1] == wrds2[j-1]) ? 0 : scost;
+			const float mn = std::min(dist_matrix[i-1][j]+idcost,dist_matrix[i][j-1]+idcost); 
 			dist_matrix[i][j] = (mn < dist_matrix[i-1][j-1]+cost) ? mn : dist_matrix[i-1][j-1]+cost; 
 		}
 	}
-	float edist = dist_matrix[len1][len2];
+	const float edist = dist_matrix[len1][len2];
 	for(int i = 0; i <= len1; i++)
 		delete[] dist_matrix[i];
 	delete[] dist_matrix;
@@ -74,33 +74,31 @@ float editdistance(const char* a, const char* b) {
 	return edist - len1 - len2;
 }
 
-// Function that determines how to convert the edit distance into 
-// a score that increases with better matches.
+// Convert the edit distance into a score that increases with better matches.
 int convert2score(float dist) {
-	int score;
-	score = 500/(dist + 5);
-	return score;
+	return int(500.0 / (dist + 5.0));
 }
 
 int main(int argc, char** argv) {
 	if(argc != 2) {
 		cerr << "Usage: " << argv[0] << " <file with Turker transcriptions>\n";
-		exit(1);
+		return 1;
 	}
 
 	ifstream ifile;
 	ifile.open(argv[1]);
 	if(!ifile) {
 		cerr << "Could not open ifile" << argv[1] << "\n";
-		exit(1);
+		return 1;
 	}
-	string line;
-	vector<pair<float,int> > turkscores;
+
 	float** turk_matrix = new float* [MAXTURKERS];
 	for(int i = 0; i < MAXTURKERS; i++)
 		turk_matrix[i] = new float[MAXTURKERS];
 
+	vector<pair<float,int> > turkscores; // why not double?
 	while(!ifile.eof()) {
+		string line;
 		getline(ifile, line);
 		if(!line.empty()) {
 			vector<string> entries = split(line, ':');
@@ -115,31 +113,27 @@ int main(int argc, char** argv) {
 					turk_matrix[k][i] = turk_matrix[i][k];
 				}
 			}
-			
-			for(unsigned i = 0; i < turktranscripts.size(); i++) {
-				turkscores[i] = make_pair(0,i);
-				for(unsigned j = 0; j < turktranscripts.size(); j++) {
-					if (i != j )
+			for (unsigned i = 0; i < turktranscripts.size(); i++) {
+				turkscores[i] = make_pair(0.0, i);
+				for (unsigned j = 0; j < turktranscripts.size(); j++) {
+					if (i != j)
 						turkscores[i].first += turk_matrix[i][j];
 				}
 			}
-			
-			std::sort(turkscores.begin(),turkscores.end());
-			
-			double bestscore = turkscores[0].first;
-			double worstscore = turkscores[turkscores.size()-1].first;
-			assert (bestscore < 0);
-            float simscore = 1.0;
-			for(size_t st = 0; st < turkscores.size(); st++) {
-                if(bestscore - worstscore != 0) {
-                    simscore = (turkscores[st].first - worstscore)/(bestscore-worstscore); //print 1-indexed turker indices
-                }
+			std::sort(turkscores.begin(), turkscores.end());
+
+			const double worstscore = turkscores[turkscores.size()-1].first;
+			const double bestscore = turkscores[0].first;
+			assert(bestscore < 0.0);
+            		float simscore = 1.0;
+			for (size_t st = 0; st < turkscores.size(); ++st) {
+                		if (bestscore != worstscore)
+					simscore = (turkscores[st].first - worstscore)/(bestscore-worstscore); //print 1-indexed turker indices
 				cout << "," << turkscores[st].second + 1 << ":" << simscore; //print 1-indexed turker indices
 			}
 
 			cout << endl;
 		}
 	}
-
 	ifile.close();
 }
