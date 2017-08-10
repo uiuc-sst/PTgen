5df143d77a63945e1d73d3f675e3405b6698de61
Camille Goudeseune <cog@illinois.edu>
2017-08-10 16:11:47 -0500

    diagnostics; commented-out debugging

--- steps/decode_PTs.sh
+++ steps/decode_PTs.sh
@@ -3,44 +3,53 @@
 . $INIT_STEPS
 
 if [[ -z $makeGTPLM && -z $decode_for_adapt ]]; then
-	>&2 echo "decode_PTs.sh: aborting.  Evaluation mode requires flag makeGTPLM=1."
-	exit 1
+  >&2 echo "$0: evaluation mode lacks flag makeGTPLM=1.  Aborting."
+  exit 1
 fi
 
 mkdir -p $decodelatdir
 
 if [[ -n $decode_for_adapt ]]; then
-	splitids=$splitadaptids
+  splitids=$splitadaptids
 else
-	splitids=$splittestids
+  splitids=$splittestids
 fi
 
 showprogress init 5 "" # Long description is in caller, ../run.sh.
 for ip in `seq -f %02g $nparallel`; do
-	(
-	for uttid in `cat $splitids.$ip`; do
-		if [[ ! -s $mergefstdir/$uttid.M.fst.txt ]]; then
-			>&2 echo -e "decode_PTs.sh: omitted $uttid because of missing file $mergefstdir/$uttid.M.fst.txt."
-			# That file might exist and be empty, but usually it is just missing.
-			continue
-		fi
-		showprogress go
+  (
+  for uttid in `cat $splitids.$ip`; do
+    if [[ ! -s $mergefstdir/$uttid.M.fst.txt ]]; then
+      >&2 echo -e "`basename $0`: omitting $uttid because of missing file $mergefstdir/$uttid.M.fst.txt."
+      # That file might exist and be empty, but usually it is just missing.
+      continue
+    fi
+    showprogress go
 
-		scale-FST-weights.pl $Mscale < $mergefstdir/$uttid.M.fst.txt \
-			| fstcompile --isymbols=$engalphabet --osymbols=$engalphabet \
-			> $mergefstdir/$uttid.M.fst
+    scale-FST-weights.pl $Mscale < $mergefstdir/$uttid.M.fst.txt \
+      | fstcompile --isymbols=$engalphabet --osymbols=$engalphabet \
+      > $mergefstdir/$uttid.M.fst
 
-		if [[ -n $makeGTPLM ]]; then
-			fstcompose $GTPLfst $mergefstdir/$uttid.M.fst \
-				| fstproject --project_output=false - > $decodelatdir/$uttid.GTPLM.fst
-		fi
+    if [[ -n $makeGTPLM ]]; then
+      fstcompose $GTPLfst $mergefstdir/$uttid.M.fst | fstproject --project_output=false - > $decodelatdir/$uttid.GTPLM.fst
+#     if [ `fstinfo $decodelatdir/$uttid.GTPLM.fst |grep "# of states" | awk 'NF>1{print $NF}'` = "0" ]; then
+#	>&2 echo -e "`basename $0`: made empty $decodelatdir/$uttid.GTPLM.fst."
+#	echo "details for $GTPLfst $mergefstdir/$uttid.M.fst composed:"
+#	fstinfo $GTPLfst | head -6 | tail -2
+#	fstinfo $mergefstdir/$uttid.M.fst | head -6 | tail -2
+#	fstcompose $GTPLfst $mergefstdir/$uttid.M.fst | fstinfo | head -6 | tail -2
+#     fi
+    fi
 
-		if [[ -n $makeTPLM ]]; then
-			fstcompose $TPLfst $mergefstdir/$uttid.M.fst \
-				| fstproject --project_output=false - > $decodelatdir/$uttid.TPLM.fst
-		fi
-	done
-	) &
+    if [[ -n $makeTPLM ]]; then
+      fstcompose $TPLfst $mergefstdir/$uttid.M.fst | fstproject --project_output=false - \
+	> $decodelatdir/$uttid.TPLM.fst
+#     if [ `fstinfo $decodelatdir/$uttid.TPLM.fst |grep "# of states" | awk 'NF>1{print $NF}'` = "0" ]; then
+#	>&2 echo -e "`basename $0`: made empty $decodelatdir/$uttid.TPLM.fst."
+#     fi
+    fi
+  done
+  ) &
 done
 wait
 showprogress end
