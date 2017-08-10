d3b250f52df55edda9fbd6013645e1056c45a0d3
Camille Goudeseune <cog@illinois.edu>
2017-08-10 15:39:03 -0500

    comments

--- util/addloop.pl
+++ util/addloop.pl
@@ -1,26 +1,27 @@
 #!/usr/bin/perl
 
-# Add zero-weighted self-loops to every state of an FST, with arguments as labels.
+# Text filter that adds zero-weighted self-loops to each state of an FST, with arguments as labels.
 
-if (@ARGV == 0) {
-	print "Usage: addloop.pl <symbol> [...]\n";
-	print "Input: FST text file";
-	exit 1;
-}
+die "Usage: $0 <symbol> [...] < in.fst.fxt > out.fst.txt\n" if @ARGV == 0;
 
+# Implement a set with a hash.  A perl idiom.
 %states=();
+
 while($line=<STDIN>) {
 	chomp;
-	@fields = split(/\s+/,$line);
+	@fields = split(/\s+/, $line);
 	if ($#fields > 2) {
+		# Accumulate the FST's states into $states.
 		$s1 = $fields[0];
 		$s2 = $fields[1];
 		$states{$s1} = 1;
 		$states{$s2} = 1;
 	}
+	# Copy everything from the original FST.
 	print "$line";
 }
 
+# Append the zero-weighted self-loops.
 foreach $s (keys %states) {
 	foreach $sym (@ARGV) {
 		print "$s $s $sym $sym 0\n";
