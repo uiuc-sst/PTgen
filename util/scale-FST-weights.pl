511a95b4ea274c3691968a886ae144a227767b51
Camille Goudeseune <cog@illinois.edu>
2017-08-10 15:45:28 -0500

    diagnostics

--- util/scale-FST-weights.pl
+++ util/scale-FST-weights.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl
 
-# Scale the weights of an FST.
+# A filter to scale the weights of an FST in OpenFST text format.
 
-die "Usage: scale-FST-weights.pl <scale>" if $#ARGV != 0;
+die "Usage: $0 scaleValue < in.fst.txt > out.fst.txt" if $#ARGV != 0;
 $scale=$ARGV[0];
 
 while(<STDIN>) {
-	chomp;
-	@fields = split(/\s+/);
-	if($#fields == 4) {
-		# arc with weight
-		$scaledwt = $fields[4] * $scale;
-		print "$fields[0]\t$fields[1]\t$fields[2]\t$fields[3]\t$scaledwt\n";
-	} elsif($#fields == 1) {
-		# weighted final state
-		$scaledwt = $fields[1] * $scale;
-		print "$fields[0]\t$scaledwt\n";
-	} else {
-		print "$_\n";
-	}
+  chomp;
+  @fields = split(/\s+/);
+  if($#fields == 4) {
+    # weighted arc
+    $scaledwt = $fields[4] * $scale;
+    print "$fields[0]\t$fields[1]\t$fields[2]\t$fields[3]\t$scaledwt\n";
+  } elsif($#fields == 1) {
+    # weighted final state
+    $scaledwt = $fields[1] * $scale;
+    print "$fields[0]\t$scaledwt\n";
+  } else {
+    print "$_\n";
+  }
 }
