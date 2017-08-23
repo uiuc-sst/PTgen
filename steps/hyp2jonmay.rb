31db3b866b901d5985bd4c78e3e3f62353b349be
Camille Goudeseune <cog@illinois.edu>
2017-08-23 15:19:55 -0500

    IL5_EVAL; Jon's filename format

--- steps/hyp2jonmay.rb
+++ steps/hyp2jonmay.rb
@@ -3,7 +3,7 @@
 
 # Read stage 15's $hypfile.
 # Write the single text file $jonmay,
-# and the dir of utterance-files jonmaydir for Jon's flat2elisa.py.
+# and the dir of utterance-files jonmaydir to send to Jon's flat2elisa.py.
 
 if ARGV.size != 4
   STDERR.puts "Usage: #$0 jonmay_dir three_letter_language_code date_USC EXPLOCAL < hyp.txt > jonmay_hyp.txt"
@@ -16,10 +16,15 @@ scrips = Hash.new {|h,k| h[k] = []} # Map each uttid to an array of transcriptio
 
 $stdin.each_line {|l|
   l = l.split
-  s = l[1..-1].join(" ")			# иисусу хоста иаа
+  s = l[1..-1].join(" ")				# иисусу хоста иаа
   next if s.empty?
-  uttid = l[0][0..10]				# RUS_134_004
-  starttime = l[0][12..-1].sub(/_.*/, '').to_i	# 101824705
+  if true						# todo: find the penultimate _ and split there.
+    uttid = l[0][0..15]					# IL5_EVAL_019_012
+    starttime = l[0][17..-1].sub(/_.*/, '').to_i	# 101824705
+  else
+    uttid = l[0][0..10]					# RUS_134_004
+    starttime = l[0][12..-1].sub(/_.*/, '').to_i	# 101824705
+  end
   scrips[uttid] << [starttime, s]
 }
 scrips = scrips.to_a.sort_by {|uttid,ss| uttid}
@@ -29,19 +34,20 @@ scrips.each {|uttid,ss| puts "#{uttid} #{ss}"}
 
 $sourceLanguage = ARGV[1]
 $genre = "SP" # "SP"eech
-$provenance = "000000" # Media outlet.  Might be in the files given to UIUC.
+$provenance = "000000" # Media outlet.  Unknown.
 $date = ARGV[2] # "20170817"
 $langForJon = $sourceLanguage.downcase
 
 `rm -rf #$jonmaydir; mkdir #$jonmaydir`
 scrips.each {|uttid,ss|
-  indexID = uttid.gsub /[_a-zA-Z]/, ""
+  indexID = uttid[8..-1].gsub /[_a-zA-Z]/, ""
   name = "#{$sourceLanguage}_#{$genre}_#{$provenance}_#{$date}_#{indexID}"
   File.open("#$jonmaydir/#{name}.txt", "w") {|f| f.puts ss}
 }
 
-$tojon = "#$EXPLOCAL/#$langForJon.eval-asr-uiuc.xml"
-puts "/ws/ifp-53_1/hasegawa/data/lorelei/PTgen/test/mcasr-rus/flat2elisa/flat2elisa.py -i #$jonmaydir -l #$langForJon -o #$tojon"
+$version = "2" # Increment this for each sftp.
+$tojon = "#$EXPLOCAL/elisa.#{$langForJon}-eng.eval-asr-uiuc.y2r1.v#$version.xml"
+
 `/ws/ifp-53_1/hasegawa/data/lorelei/PTgen/test/mcasr-rus/flat2elisa/flat2elisa.py -i #$jonmaydir -l #$langForJon -o #$tojon`
-`rm -rf #$jonmaydir #$tojon.gz; gzip --best #$tojon`
+`rm -rf #$tojon.gz; gzip --best #$tojon` # Also rm #$jonmaydir
 STDERR.puts "Please sftp to Jon the file #$tojon.gz." # See aug/a.txt.
