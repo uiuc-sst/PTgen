4ce548412ed03d6039cea45958b2d054976e8728
Camille Goudeseune <cog@illinois.edu>
2017-08-28 12:44:58 -0500

    other filenames

--- steps/wordlengths.rb
+++ steps/wordlengths.rb
@@ -3,7 +3,28 @@
 
 # Report the lengths of the words in a set of transcriptions.
 
-words = File.readlines("/tmp/Exp/tigrinya/hypotheses.txt.restitched.txt").map {|l| l.chomp.strip.split(' ')[1..-1]} .flatten .sort
+$in = "/tmp/Exp/tigrinya/hypotheses.txt.restitched.txt"
+$in = "/home/camilleg/l/PTgen/native-scrips-il5/transcription.txt"
+$in = "/tmp/Exp/oromo/hypotheses.txt.restitched.txt.tmp"
+$in = "/tmp/Exp/oromo/hypotheses.txt.restitched.txt"
+
+words = File.readlines($in).map {|l| l.chomp.strip.split(' ')[1..-1]} .flatten .sort
+
+# Print the alphabet.
+if false
+  letters = words.join("").split(//).uniq
+  p letters # .size == 139
+  exit 0
+end
+
+# Print the 1-letter words.
+if false
+  words.select! {|w| w.size==1}
+  words.uniq!
+  p words.sort # .size == 40
+  exit 0
+end
+
 words.map! {|w| w.size}
 a = Array.new(words.max + 1){0}
 words.each {|w| a[w] += 1} # Build histogram.
