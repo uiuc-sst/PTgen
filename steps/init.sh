f8c0dfa8b4d8c4257e881d5d368299e28e63bc94
Camille Goudeseune <cog@illinois.edu>
2017-10-02 12:17:19 -0500

    cosmetics

--- steps/init.sh
+++ steps/init.sh
@@ -3,9 +3,6 @@
 # Parse settings_file ($1) into a bunch of $variables.
 # Define some utility functions.
 
-# Exit if there is any error.
-#set -e
-
 scriptname=`basename "$0"`
 
 nparallel=`nproc | sed "s/$/-1/" | bc`	# One fewer than the number of CPU cores.
@@ -50,18 +47,15 @@ evaloutput=$EXPLOCAL/eval.txt			# Made by stage 15, read by human.
 # Make a temporary directory for the caller.
 # (Change /tmp to something else, for servers with harsh quotas on /tmp.)
 mktmpdir() {
-	export tmpdir=/tmp/$scriptname-$$.dir
-	mkdir -p $tmpdir
-	>&2 echo "Made tmpdir $tmpdir."
+  export tmpdir=/tmp/$scriptname-$$
+  mkdir -p $tmpdir
+  >&2 echo "Made tmpdir $tmpdir."
 }
 
 # Verify that a previously created file exists, and report that.
 usingfile() {
-	if [[ ! -e $1 ]]; then
-		>&2 echo "$scriptname: no file \"$1\" for $2. Aborting."
-		exit 1
-	fi
-	>&2 echo "Reusing $2 $1."
+  [[ -e $1 ]] || { >&2 echo "$scriptname: no file \"$1\" for $2. Aborting."; exit 1; }
+  >&2 echo "Reusing $2 $1."
 }
 
 # Print a row of dots.
@@ -69,24 +63,24 @@ usingfile() {
 # showprogress go:				possibly print another dot.
 # showprogress end:				print "Done."
 #
-# To show an error message more noticeably at the start of a line instead of
+# To show an error message more noticeably at the start of a line, instead of
 # mid-line after the most recent dot, use echo -e "\nError message."
 showprogress() {
-	local arg="$1"
-	case $arg in
-		init)
-			__progress_counter__=1
-			__progress_stepsize__=$2
-			>&2 echo -n "$3..."
-			;;
-		go)
-			if [[ $((__progress_counter__ % __progress_stepsize__)) -eq 0 ]] ; then
-				>&2 echo -n "."
-			fi
-			((__progress_counter__++))
-			;;
-		end)
-			>&2 echo " Done."
-			;;
-	esac
+  local arg="$1"
+  case $arg in
+    init)
+      __progress_counter__=1
+      __progress_stepsize__=$2
+      >&2 echo -n "$3..."
+      ;;
+    go)
+      if [[ $((__progress_counter__ % __progress_stepsize__)) -eq 0 ]] ; then
+	      >&2 echo -n "."
+      fi
+      ((__progress_counter__++))
+      ;;
+    end)
+      >&2 echo " Done."
+      ;;
+  esac
 }
