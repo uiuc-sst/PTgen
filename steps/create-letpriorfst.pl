13d7c94f4720c95df76fec42543bed61d176854f
Camille Goudeseune <cog@illinois.edu>
2017-10-24 13:07:19 -0500

    clarify

--- steps/create-letpriorfst.pl
+++ steps/create-letpriorfst.pl
@@ -2,25 +2,28 @@
 
 # Create a prior over letters and represent it as an FST.
 
+use File::Basename;
+my $name = basename($0);
+
 if ($#ARGV != 1) {
-	print "Usage: $0 <merged transcripts dir> <uttids>\n";
+	print "Usage: $name <merged transcripts dir> <uttids>\n";
 	exit 1;
 }
 
-$mergedir = $ARGV[0]; # $mergedir
+$mergedir = $ARGV[0];
 $uttlist  = $ARGV[1]; # $trainids
-open(UTT, $uttlist) or die "$0: failed to open list of utterances $uttlist\n";
+open(UTT, $uttlist) or die "$name: failed to open list of utterances $uttlist\n";
 
 %labelcount = ();
 $total = 0;
-while($utt = <UTT>) {
+while ($utt = <UTT>) {
 	chomp($utt);
 	$filename = "$mergedir/$utt".".txt";
-	open(FILE, "$filename") or print STDERR "$0 failed to open $filename.\n";
-	while($line = <FILE>) {
+	open(FILE, "$filename") or print STDERR "$name: failed to open $filename.\n";
+	while ($line = <FILE>) {
 		chomp;
 		$line =~ s/\s+$//; $line =~ s/^\s+//;
-		@labels = split(/\s+/,$line);
+		@labels = split(/\s+/, $line);
 		$labelcount{$_}++ for @labels;
 		$total += $#labels + 1;
 	}
@@ -29,13 +32,13 @@ while($utt = <UTT>) {
 close(UTT);
 
 if ($total == 0) {
-  print STDERR "$0 read nothing, so it can't make L.fst.";
+  print STDERR "$name read nothing, so it can't make L.fst.";
   exit 1;
-  # Were we to continue, we'd fail at log(0).
+  # Were we to continue, log(0) would fail.
 }
 $logtot = log($total);
-foreach $let (keys %labelcount) {
-	$wt = log($labelcount{$let}) - $logtot;
-	print "0 0 $let $let $wt\n";
+foreach $letter (keys %labelcount) {
+	$weight = log($labelcount{$letter}) - $logtot;
+	print "0 0 $letter $letter $weight\n";
 }
 print "0\n";
