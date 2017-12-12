71a309344e1959782760e4dd8a87bbd97272f0c2
Camille Goudeseune <cog@illinois.edu>
2017-12-12 12:37:47 -0600

    also handle fstrandgen's output

--- util/reverse_fst_path.pl
+++ util/reverse_fst_path.pl
@@ -1,6 +1,18 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
-# Print, as a string, a shortest-path FST's output labels (from "fstprint --osymbols").
+# Print, as a string, an FST's output labels (from "fstprint --osymbols").
+# When processing the output of fstshortestpath, pass this the option --shortestpath.
+# When processing the output of fstrandgen --select=log_prob, don't.
+
+$shortestpath = 0;
+while (@ARGV) {
+  if ($ARGV[0] eq "--shortestpath") {
+    $shortestpath = 1;
+  } else {
+    print STDERR "$0: ignoring unexpected argument: $ARGV[0]\n";
+  }
+  shift @ARGV;
+}
 
 @arcs = ();
 binmode STDIN, ':utf8';
@@ -10,9 +22,10 @@ while(<STDIN>) {
   push(@arcs, $_);
 }
 
-# Accumulate output labels $l into $olabel_seq.
+# Append output labels $l into $olabel_seq.
 $olabel_seq = "";
-for($a = 0; $a != -1; ($a==0? $a=$#arcs : ($a==1 ? $a=-1 : $a--))) {
+
+sub accumulate {
   @fields = split(/\s+/, $arcs[$a]);
   $l = "";
   if($#fields > 2) {
@@ -24,8 +37,16 @@ for($a = 0; $a != -1; ($a==0? $a=$#arcs : ($a==1 ? $a=-1 : $a--))) {
      $l ne "sil" &&
      $l ne "spn" &&
      $l !~ "#") {
-       $olabel_seq = "$olabel_seq $l";
+       $olabel_seq .= " $l";
   }
+}
+
+if ($shortestpath) {
+  # fstshortestpath's output's order is peculiar.
+  for($a = 0; $a != -1; ($a==0? $a=$#arcs : ($a==1 ? $a=-1 : $a--))) { accumulate(); }
+} else {
+  # fstrandgen's output's order is unastonishing.
+  for($a = 0; $a < $#arcs; $a++) { accumulate(); }
 }
 
 $olabel_seq =~ s/^\s+//g;
